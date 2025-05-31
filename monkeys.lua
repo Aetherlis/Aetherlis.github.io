8eb67955d12c7625c86b3b6fd11c1cf0f34926cc
xyzzr <110189904+xyzzr@users.noreply.github.com>
2025-06-01 00:02:44 +0300

    Create monkeys.lua

---
+++ monkeys.lua
@@ -0,0 +1,8 @@
+local monkeys = {
+    {name = "FlaseVanned", ID = 4457948881},
+    {name = "z47h", ID = 537232828},
+    {name = "sarrowfr", ID = 366196},
+    {name = "Scipths", ID = 2489061587},
+    {name = "tiimiings", ID = 7399778587},
+    {name = "swerveisabottom", ID = 8576849860},
+}
